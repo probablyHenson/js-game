5c2bbcea00f9bbbd85f0beeb6db3a3143c5de432
probablyHenson <probablyhenson@gmail.com>
2021-05-21 13:31:16 -0400

    created game loop

--- game.js
+++ game.js
@@ -1,15 +1,36 @@
-const canvas = document.getElementById('screen');
-const ctx = canvas.getContext('2d');
+const game = { };
 
-function setScreenColor(color) {
+function mainLoop(lastTick) {
+    if (!lastTick) lastTick = window.performance.now();
+    window.requestAnimationFrame(mainLoop);
+    game.setScreenColor('black');
+    game.updateTime(lastTick);
+}
+
+game.setScreenColor = function(color) {
+    const { canvas, ctx } = this;
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function setScreenSize(width, height) {
+game.setScreenSize = function (width, height) {
+    const { canvas } = this;
     canvas.setAttribute('width', width);
-    canvas.setAttribute('height',  height);
+    canvas.setAttribute('height', height);    
 }
 
-setScreenSize('640px', '480px');
-setScreenColor('black');
+game.start = function() {
+    const canvas = document.getElementById('screen');
+    const ctx = canvas.getContext('2d');
+    Object.assign(this, { canvas, ctx });
+    this.setScreenSize('640px', '480px');
+    mainLoop();
+}
+
+game.updateTime = function(lastTick) {
+    if (!game.lastTick) game.lastTick = lastTick;
+    this.tickDelta = lastTick - game.lastTick;
+    this.lastTick = lastTick;
+}
+
+game.start();
